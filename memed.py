656d53ce6282c51711357f72df112b0d8bcb811d
filedesless <filedesless@gmail.com>
2017-10-12 20:15:53 -0400

    Added meme-file and loop

--- memed.py
+++ memed.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 import socket
 from random import randint
 
@@ -8,12 +8,20 @@ MEMED_PORT = 9001
 
 # TODO: Retrieve the memes from a datasource
 # TODO: Allow different types of memes (audio, gif, text, img...)
-memes = ["Generic meme \#1", "Generic meme \#2"]
+memes = []
+
+with open("memes.txt") as f:
+    memes = f.readlines()
 
 # TODO: Allow UNIX socket usage
-memed = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-memed.bind((MEMED_IP, MEMED_PORT))
-memed.listen(1)
+with socket.socket() as memed:
+    # Prevents "address already in use"
+    memed.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    memed.bind((MEMED_IP, MEMED_PORT))
+    memed.listen(5)
 
-conn, addr = memed.accept()
-conn.send(memes[randint(0, 1)])
+    while True:
+        # Discards remote address to close connection
+        with memed.accept()[0] as conn:
+            le_meme = memes[randint(0, 1)]
+            conn.send(le_meme.encode())
